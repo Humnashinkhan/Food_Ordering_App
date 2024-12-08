3c79dc1ee96f2acacfdef1f8f3de39fdc75efd8a
Humnashin Khan <humnashin9955@gmail.com>
2024-12-08 13:14:32 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="h-20 bg-slate-950">
-       Header
-
+    <div className="h-24 bg-white shadow-lg">
+        <div className="flex justify-between">
+          <img
+           className="h-28 p-4 ml-8"
+           alt="logo"
+           src="https://i.pinimg.com/736x/84/4d/85/844d8535fb36b860d3a16635ef20fbab.jpg"
+           />
+        <div className="mr-10">
+             <ul className="flex">
+              <li className="px-3 py-2 rounded-lg mt-8 font-bold text-gray-500 hover:bg-[#d97919]">Home</li>
+              <li className="px-3 py-2 rounded-lg mt-8 font-bold text-gray-500 hover:bg-[#d97919]">About</li>
+              <li className="px-3 py-2 rounded-lg mt-8 font-bold text-gray-500 hover:bg-[#d97919]">Contact</li>
+              <li className="px-3 py-2 rounded-lg mt-8 font-bold text-gray-500 hover:bg-[#d97919]">Login</li>
+            </ul>
+           </div>
+           </div>
     </div>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="">
+        <input
+        className="border-2 w-[450px] h-9 mt-8 ml-96 outline-[#cead8d] p-3" 
+        type="text" 
+        placeholder="Search a restaurant you want.." 
+        />
+        <button
+        className="h-9 bg-[#c26100] text-white w-20 rounded-r-md"
+        >
+        Search</button>
+
+    </div>
+  )
+}
+
+export default Home

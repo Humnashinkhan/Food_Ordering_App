ade505ea609e5d496ab1a709e1f1739ab7d18483
Humnashin Khan <humnashin9955@gmail.com>
2024-12-13 14:28:36 +0530

    aboutus section with css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div  className="flex justify-between px-36 py-24">
+    <div  className="flex justify-between px-56 py-24">
     <div className="">
-      <h1 className="text-[#545454] font-bold text-5xl">Welcome to<br/>
+      <h1 className="text-[#545454] font-bold text-[60px]">Welcome to<br/>
       The world of
     </h1>
-       <p>Tasty & Fresh Food</p>
-       <p>"Better you will feel if you eat a FoodFire healthy meal"
+       <p className="text-[60px] font-bold bg-[#d97919] text-white px-4 rounded-lg">Tasty & Fresh Food</p>
+       <p className="text-[20px] italic font-semibold text-[#545454]">"Better you will feel if you eat a FastFood healthy meal"
        </p>
     </div>
     <div className="">

9decc5d78fc54b4c256ca267c104d99c8fe71069
Humnashin Khan <humnashin9955@gmail.com>
2024-12-15 15:20:28 +0530

    write responsive code for aboutUs componenets

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div  className="flex justify-between px-56 py-24">
+    <div  className="h-[700px] md:h-[500px] md:flex justify-between px-8 md:px-56 py-24">
     <div className="">
-      <h1 className="text-[#545454] font-bold text-[60px]">Welcome to<br/>
+      <h1 className="text-[#545454] font-bold text-[25px] md:text-[60px]">Welcome to<br/>
       The world of
     </h1>
-       <p className="text-[60px] font-bold bg-[#d97919] text-white px-4 rounded-lg">Tasty & Fresh Food</p>
-       <p className="text-[20px] italic font-semibold text-[#545454]">"Better you will feel if you eat a FastFood healthy meal"
+       <p className="text-[25px] md:text-[60px] font-bold bg-[#d97919] text-white px-4 rounded-lg">Tasty & Fresh Food</p>
+       <p className="text-[15px] md:text-[20px] italic font-semibold text-[#545454]">"Better you will feel if you eat a FastFood healthy meal"
        </p>
     </div>
     <div className="">

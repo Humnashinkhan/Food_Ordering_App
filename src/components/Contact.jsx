c1156a89a172acfd6148585f330fc21f69d88e8f
Humnashin Khan <humnashin9955@gmail.com>
2024-12-17 11:57:50 +0530

    make responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className="h-[500px] flex justify-between">
-      <div className="ml-72 pt-16">
+    <div className="md:h-[500px] md:flex justify-between">
+      <div className="md:ml-72 pt-16 ml-10">
         <img 
          alt="img"
          src="https://foodfire-app.netlify.app/contactUs.6d5a1f67.png"
          />
       </div>
       <form>
-      <div className="grid gap-5 pt-20 mr-64">
+      <div className="grid gap-5 md:pt-20 p-5 md:mr-64">
          <h1 className="text-[#545454] text-center font-bold text-[30px]">Contact Us</h1>
         <input
            className="md:w-[30rem] p-2 border-2 border-gray-200 focus:border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-200" 
@@ -34,7 +34,7 @@ const Contact = () => {
            required
            />
           <input 
-          className="bg-[#d97919] text-white rounded-md hover:bg-[#9c5813] w-40 ml-36 py-2"
+          className="bg-[#d97919] text-white rounded-md hover:bg-[#9c5813] w-40 ml-24 md:ml-36 py-2"
           type="submit"
           value="Send Message"
           />
